a84f5a8a3a45565ea2d05f2478cb93b88a162d07
Nimrod Shneor <nimrodshn@gmail.com>
2015-05-18 14:29:53 +0300

    Changes in DataSetOrginizer

--- python/dataSetOrginizer.py
+++ python/dataSetOrginizer.py
@@ -10,58 +10,18 @@ class dataSetOrginizer:
        defaultSet = np.load('Data.npz')
        self._dataSets.append(defaultSet)
 
-    def createDefaultTrainingSet(self):
+
+    def InputDataset(self,name):
         '''
-        Creates the Default Training Model
-        :return: Void
+        Inputs The Images for Training set to be created.
+        :param name:
+        :return:
         '''
 
-        address = "..//data//training//Default"
-        labels = []
-        trainingData = []
-        classes = []
-        cl = 0
-        for items in os.listdir(address):
 
-            name = address + "//" + items
-            labels.append(items)
-            #print items
-
-            for it in os.listdir(name):
-
-                path = name + "//" + it
-                #print path # DEBUG
-
-                img = cv.imread(path)
-
-                orb = cv.ORB()
-                kp = orb.detect(img,None)
-
-                ## Normalize the Data, taking only Data with 50 KeyPoints
-                if len(kp) > 15:
-                    kp = kp[:15]
-                    kp, des = orb.compute(img, kp)
-
-                    '''
-                    ## DEBUG ##
-                    im2 = cv.drawKeypoints(img ,kp,color=(0,255,0), flags=0)
-                    cv.namedWindow(path, cv.WINDOW_NORMAL)
-                    cv.imshow(path, im2)
-                    '''
-
-                ####### Transformations on the Array #######
-                    d=des.flatten()
-                    trainingData.append(d)
-                    classes.append(cl)
-
-            cl = cl + 1
-
-        np.savez('Data',trainingData,labels, classes)
-
-
-    def createNewTrainingSet(self, name, location=''):
+    def createTrainingFromDataset(self, name, location=''):
         '''
-          Creats a new training set to work on
+        Creates a new training set to work on from given dataset in location: creating Feature Vector, Normalize etc..
         :param name: the name of the data set
         :param location: location where the data was collected
         :return:
